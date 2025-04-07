d8d79ae078d7aacb69fba350e0e793718cd12ba0
Marius Paduraru <marius.paduraru@gmail.com>
2025-04-07 19:38:20 +0100

    Add comments management functionality and configuration files

---
+++ app/Comments.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace EssexMums\Theme;
+
+class Comments {
+    public function __construct() {
+        // Hide comments from the admin menu
+        add_action( 'admin_menu', [ $this, 'hide_comments' ] );
+        // Disable comments on the front end
+        add_filter( 'comments_open', [ $this, 'disable_comments_status' ], 20, 2 );
+        // Disable comments in the admin menu
+        add_action( 'admin_menu', [ $this, 'disable_comments_admin_menu' ] );
+        // Disable comments in the admin bar
+        add_action( 'admin_bar_menu', [ $this, 'disable_comments_admin_bar' ], 999 );
+        // Disable comments support for all post types
+        add_action( 'init', [ $this, 'disable_comments_post_types_support' ] );
+    }
+
+
+    public function hide_comments(): void {
+        // Hide comments from the admin menu
+        remove_menu_page( 'edit-comments.php' );
+        // Hide comments from the admin bar
+        add_action( 'admin_bar_menu', function ( $wp_admin_bar ) {
+            $wp_admin_bar->remove_node( 'comments' );
+        }, 999 );
+    }
+
+    public function disable_comments_post_types_support(): void {
+        $post_types = get_post_types();
+        foreach ( $post_types as $post_type ) {
+            remove_post_type_support( $post_type, 'comments' );
+            remove_post_type_support( $post_type, 'trackbacks' );
+        }
+    }
+
+
+    public function disable_comments_status(): bool {
+        return false;
+    }
+
+    public function disable_comments_admin_menu(): void {
+        remove_menu_page( 'edit-comments.php' );
+    }
+
+    public function disable_comments_admin_bar(): void {
+        add_action( 'admin_bar_menu', function ( $wp_admin_bar ) {
+            $wp_admin_bar->remove_node( 'comments' );
+        }, 999 );
+    }
+
+}
--- functions.php
+++ functions.php
@@ -5,8 +5,10 @@ namespace EssexMums\Theme;
 // Define a constant 'WPT_DIR' which holds the absolute path to the directory of the current file.
 // The function get_template_directory() is used to get the absolute path to the directory.
 // This constant can be used throughout the plugin to reference files and directories relative to the root directory of the plugin.
-define('WPT_DIR', get_template_directory());
+define( 'WPT_DIR', get_template_directory() );
 
-require_once(WPT_DIR . '/app/ThemeManager.php');
+require_once( WPT_DIR . '/app/ThemeManager.php' );
+require_once( WPT_DIR . '/app/Comments.php' );
 
 new ThemeManager();
+new Comments();
